ec02c15b9ca32579d6695981df6e8cb3c5d0b036
sanoje123 <pera.putin52@gmail.com>
2026-03-10 20:29:03 +0100

    Function for adding images.

--- app/app.py
+++ app/app.py
@@ -4,6 +4,11 @@ from app.db import create_db_and_tables, get_async_session, Post
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
 from sqlalchemy import select
+from app.images import imagekit
+import shutil
+import os
+import uuid
+import tempfile
 
 @asynccontextmanager
 async def lifespan(app:FastAPI):
@@ -18,17 +23,38 @@ async def upload_file(
     caption: str = Form(""),    #caption comes from a form field, not JSON
     session: AsyncSession = Depends(get_async_session)  #Before running this function, give me a database session.
 ):
-    post = Post(
-        caption=caption,
-        url="dummy url",
-        file_type="photo",
-        file_name="name"
-    )
+    temp_file_path = None
     
-    session.add(post)   #This tells SQLAlchemy:"Add this object to the transaction." - It does NOT write to database yet.
-    await session.commit()  #Now SQLAlchemy sends the query to the database. Equivalent SQL: INSERT INTO post (...) VALUES (...)
-    await session.refresh(post) #After commit, the post object is updated with any new data from the database, such as the generated id. This is important for getting the id of the newly created post.
-    return post
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(file.filename)[1]) as temp_file:
+            temp_file_path = temp_file.name
+            shutil.copyfileobj(file.file, temp_file)
+        
+        with open(temp_file_path, 'rb') as f:
+            upload_resut = imagekit.files.upload(
+                file=f,
+                file_name=file.filename,
+                use_unique_file_name=True,
+                tags=["backend-upload"]
+            )
+            
+        post = Post(
+                caption=caption,
+                url=upload_resut.url,
+                file_type="video" if file.content_type.startswith("video/") else "image",
+                file_name=upload_resut.name
+            )
+        session.add(post)   #This tells SQLAlchemy:"Add this object to the transaction." - It does NOT write to database yet.
+        await session.commit()  #Now SQLAlchemy sends the query to the database. Equivalent SQL: INSERT INTO post (...) VALUES (...)
+        await session.refresh(post) #After commit, the post object is updated with any new data from the database, such as the generated id. This is important for getting the id of the newly created post.
+        return post 
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        if temp_file_path and os.path.exists(temp_file_path):
+            os.unlink(temp_file_path)
+        await file.close()
 
 @app.get("/feed")
 async def get_feed(
@@ -41,7 +67,7 @@ async def get_feed(
     for post in posts:
         posts_data.append({
             "id":str(post.id),
-            "captiopn":post.caption,
+            "caption":post.caption,
             "url":post.url,
             "file_type":post.file_type,
             "file_name":post.file_name,
